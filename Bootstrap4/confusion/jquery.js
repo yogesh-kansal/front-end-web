415d7ee1a00f3c57ebb0b930054319efc36a1497
yogesh028-coder <yogeshiitbhub@gmail.com>
2020-10-28 23:50:55 +0530

    added hints

---
+++ Bootstrap4/confusion/jquery.js
@@ -0,0 +1,12 @@
+syntax:-   $(selector).action();
+
+$(document).readyy(function() {
+    $('[attribute]').action();
+});
+// accessing HTML element:- "p", "button", "h4"....
+//            by id:-       #id    ==> #click
+//            by class:-    .class ==> .btn ,btn-default
+//            attribute:-   "[]"  ==> "[href]", "[data-toggle="tooltip"]"
+//current elements          $(this).
+
+
