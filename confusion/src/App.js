2b10156ce0952ec320fd34370dc894035f4946e6
yogesh028-coder <yogeshiitbhub@gmail.com>
2020-10-30 01:01:17 +0530

    added menu component(1st component)

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,6 +1,28 @@
-import logo from './logo.svg';
+import React, {Component } from 'react';
+//import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/menucomp';
 import './App.css';
 
+class App extends Component {
+  render() {
+    return (
+      <div /*className="App"*/>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="#">React web App</NavbarBrand>
+
+          </div>
+        </Navbar>
+        <Menu />
+      </div>
+    );
+  }
+}
+
+
+
+/*
 function App() {
   return (
     <div className="App">
@@ -8,6 +30,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          now date is { new Date().toLocaleDateString()}{<br></br>}  tiem is {new Date().toLocaleTimeString()}
         </p>
         <a
           className="App-link"
@@ -21,5 +44,5 @@ function App() {
     </div>
   );
 }
-
+*/
 export default App;
